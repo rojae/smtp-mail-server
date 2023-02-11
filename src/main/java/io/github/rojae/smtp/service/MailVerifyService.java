9bd51f111980118d43616d5450f64aa7c874241a
rojae <rojae@kakao.com>
2023-02-11 15:05:07 +0900

    fix. 메일 인증기능

--- src/main/java/io/github/rojae/smtp/service/MailVerifyService.java
+++ src/main/java/io/github/rojae/smtp/service/MailVerifyService.java
@@ -5,6 +5,7 @@ import io.github.rojae.smtp.common.repository.MailRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,8 +16,10 @@ public class MailVerifyService {
 
     private final MailRepository mailRepository;
 
+    @Transactional(readOnly = false)
     public boolean verifyBySecret(String secret, String email){
         Optional<Mail> bySecretKeyAndEmail = mailRepository.findBySecretKeyAndEmail(secret, email);
+        bySecretKeyAndEmail.ifPresent(mail -> mail.setIsAuth('Y'));
         return bySecretKeyAndEmail.isPresent();
     }
 
